a2c9e5d7340d13d4668f83f371c22b26b7b0191f
r4inmaker <jakobsircelj@gmail.com>
2025-04-12 14:37:23 +0200

    css dont work

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode className='w-full h-screen'>
     <App />
   </StrictMode>,
 )
