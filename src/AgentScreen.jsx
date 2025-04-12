62149878ef2754e4b7ceabc857d957398721a6b5
r4inmaker <jakobsircelj@gmail.com>
2025-04-12 22:18:08 +0200

    recording animation

--- src/AgentScreen.jsx
+++ src/AgentScreen.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import Agent from './Agent'
 import Chat from './Chat'
+import Microphone from './Microphone'
 
 export default function AgentScreen(
-   {chatRef, speaking, gender, language, flag}
+   {chatRef, speaking, listening, gender, language, flag}
 ) {
 
     return (
         <>
         <div className='w-4/5 flex items-center justify-center bg-[#1a1a1a] rounded-4xl p-10 gap-10'>
 
-            <div className='max-w-150 min-h-170 flex justify-center items-center shadow-[12px_12px_20px_rgba(0,0,0,0.4)]'>
-                <Chat ref={chatRef}/>
+            <div className='w-full h-full flex flex-col justify-between items-center gap-20'>
+                <div className='w-150 min-h-160 max-h-100 flex flex-col justify-center items-center shadow-[12px_12px_20px_rgba(0,0,0,0.4)]'>
+                    <Chat ref={chatRef} />
+                    
+                </div>
+                <Microphone isRecording={listening}/>
             </div>
 
             <div className='max-w-160 bg-h-full bg-[#272727] rounded-2xl shadow-[12px_12px_20px_rgba(0,0,0,0.4)]'>
